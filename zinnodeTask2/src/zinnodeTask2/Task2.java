3392c4643c0d927dde2e2730867ea8d39a4eea35
Mani Priya <mani@zensar.com>
2024-01-17 19:37:39 +0530

    Initial Commit

---
+++ zinnodeTask2/src/zinnodeTask2/Task2.java
@@ -0,0 +1,45 @@
+package zinnodeTask2;
+
+public class Task2 {
+
+	
+
+//1.Why do you like to join as a Software developer?
+  /*
+   As a software developer, i can enhance my skills and  i am passionate about this role.And also i am good with the needed skills 
+  like Java,SpringBoot,mysql.
+  */
+	
+
+//2.List top 3 softwares / tools that inspire you. Why do these tools inspire you?
+/*
+ 1.eclipse
+ 2.springBoot
+ 3.mySql
+ As my career started by using these tool.I learnt step by step.And built projects using these softwares
+ */
+	
+
+//3.List top 3 organisations you like to join in your career & why you like these organisations.
+	/*
+	 Microsoft
+	 google 
+	 amazon
+	 */
+
+//4.How you position yourself in this industry after 2 years.
+	/*
+	 After 2 years i want to see myself as a senior Software developer
+	 */
+	
+	
+
+//5.List 3 projects / products you have built or have planned to build in the upcoming year, also explain your role in the same.
+	/*
+	 *Atm (depositing,withdraw,checking the pin credentials)
+	 ola booking
+	 app login
+	 
+	 */
+
+}
